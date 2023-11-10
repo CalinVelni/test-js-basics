e992dea4f64806acacb8f18fcf18aab72fd696e4
Calin <velniciuc.calin@icloud.com>
2023-11-10 10:38:36 +0100

    feat | exercises folders & exercise 1 done

---
+++ Esercizio1/main1.js
@@ -0,0 +1,35 @@
+const analizzaTesto = (stringa) => {
+
+    const invertiStringa = (str) => {
+
+        let strInv = '';
+
+        for (let i = str.length - 1; i >= 0; i --){  
+
+            strInv += str[i];
+           }
+
+           return strInv;
+    }
+
+    const analisi = {
+
+        originale: stringa,
+
+        minuscolo: stringa.toLowerCase(),
+
+        senzaSpazi: stringa.trim(),
+
+        numeroCaratteri: stringa.length,
+
+        contineJS: stringa.includes('JS') || stringa.includes('JavaScript'),
+
+        invertita: invertiStringa(stringa),
+    }
+
+    return analisi;
+}
+
+console.log(analizzaTesto('  Ho studiato JavaScript  '));
+
+
